13a7ddf193a503de5cb5940e15c7ab7c760c4fb7
nahorfelix <felixnahor2@gmail.com>
2025-01-10 20:21:23 +0300

    Milstone1

--- _config.js
+++ _config.js
@@ -1,10 +1,11 @@
-// Update to have your correct username and password
-const config = {
-    mongoURI: {
-        production: 'mongodb+srv://felixnahor2:@F27920459f@nahor.8at95.mongodb.net/nahor?retryWrites=true&w=majority',
-        development: 'mongodb+srv://felixnahor2:@F27920459f@nahor.8at95.mongodb.net/nahor-dev?retryWrites=true&w=majority',
-        test: 'mongodb+srv://felixnahor2:@F27920459f@nahor.8at95.mongodb.net/nahor-test?retryWrites=true&w=majority',
-    },
-};
+var config = {}
 
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://felixnahor:felix@devops.oplwc.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://felixnahor:felix@devops.oplwc.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://felixnahor:felix@devops.oplwc.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+}
 module.exports = config;
+
+
