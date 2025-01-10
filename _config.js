db42a9641df39999b95ed2a37ef9ccd38df473ec
nahorfelix <felixnahor2@gmail.com>
2025-01-10 11:55:01 +0300

    MIlstone1

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
-var config = {}
-
 // Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+const config = {
+    mongoURI: {
+        production: 'mongodb+srv://felixnahor2:@F27920459f@nahor.8at95.mongodb.net/nahor?retryWrites=true&w=majority',
+        development: 'mongodb+srv://felixnahor2:@F27920459f@nahor.8at95.mongodb.net/nahor-dev?retryWrites=true&w=majority',
+        test: 'mongodb+srv://felixnahor2:@F27920459f@nahor.8at95.mongodb.net/nahor-test?retryWrites=true&w=majority',
+    },
+};
+
 module.exports = config;
